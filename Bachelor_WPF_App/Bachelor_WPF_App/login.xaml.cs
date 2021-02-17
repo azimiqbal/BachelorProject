3c4b4c0dc7cf4d571b5faceb06ad00536aa017c2
azimiqbal <47232822+azimiqbal@users.noreply.github.com>
2021-02-17 08:42:35 +0100

    connecting login with back end
    the log in form is now connected with the database, and added some extensions.

--- Bachelor_WPF_App/Bachelor_WPF_App/login.xaml.cs
+++ Bachelor_WPF_App/Bachelor_WPF_App/login.xaml.cs
@@ -10,6 +10,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
 
 namespace Bachelor_WPF_App
 {
@@ -23,21 +25,62 @@ namespace Bachelor_WPF_App
             InitializeComponent();
         }
 
-        private void LocalLoginButton_Click(object sender, RoutedEventArgs e)
+        private bool IsValid()
         {
-            string username = LocalUsernameBox.Text;
-            string password = LocalPasswordBox.Password;
-
-            if ((this.LocalUsernameBox.Text == "Deez") && (this.LocalPasswordBox.Password == "Nuts"))
+            if (LocalUsernameBox.Text.TrimStart() == string.Empty)
             {
-                NavigationService service = NavigationService.GetNavigationService(this);
-                service.Navigate(new Uri("testRun.xaml", UriKind.RelativeOrAbsolute));
+                MessageBox.Show("Error! Please enter a valid user name");
+                return false;
             }
-            else
+            else if (LocalPasswordBox.Password.TrimStart() == string.Empty)
             {
-                MessageBox.Show("you are not granted with access");
+                MessageBox.Show("Error! Please enter a valid password");
+                return false;
             }
+            return true;
+        }
+    
 
+    private void LocalLoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (IsValid())
+            {
+                using (SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Azim\Documents\GitHub\BachelorProject\Bachelor_WPF_App\Bachelor_WPF_App\forsvaret.mdf;Integrated Security=True"))
+                {
+                    string query = "SELECT * FROM USERS WHERE UserName = '" + LocalUsernameBox.Text.Trim() +
+                        "' AND Password = '" + LocalPasswordBox.Password.Trim() + "'";
+
+                    SqlDataAdapter sda = new SqlDataAdapter(query, conn);
+                    DataTable dta = new DataTable();
+                    sda.Fill(dta);
+                    if (dta.Rows.Count == 1)
+                    {
+                        NavigationService service = NavigationService.GetNavigationService(this);
+                        service.Navigate(new Uri("testRun.xaml", UriKind.RelativeOrAbsolute));
+                    } else
+                    {
+                        MessageBox.Show("Username or password is not correct");
+                    }
+                }
+            }
         }
     }
+
 }
+
+//private void LocalLoginButton_Click(object sender, RoutedEventArgs e)
+//{
+//    string username = LocalUsernameBox.Text;
+//    string password = LocalPasswordBox.Password;
+
+//    if ((this.LocalUsernameBox.Text == "Deez") && (this.LocalPasswordBox.Password == "Nuts"))
+//    {
+//        NavigationService service = NavigationService.GetNavigationService(this);
+//        service.Navigate(new Uri("testRun.xaml", UriKind.RelativeOrAbsolute));
+//    }
+//    else
+//    {
+//        MessageBox.Show("you are not granted with access");
+//    }
+
+//}
