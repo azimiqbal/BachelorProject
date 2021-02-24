120477dc273d66a4cbf665df36e332055ea9a0d7
azimiqbal <47232822+azimiqbal@users.noreply.github.com>
2021-02-24 13:01:59 +0100

    delete function in run test page
    added a delete function

--- VeiebryggeApplication/regnr.xaml.cs
+++ VeiebryggeApplication/regnr.xaml.cs
@@ -142,6 +142,27 @@ namespace VeiebryggeApplication
         {
             MessageBox.Show("deez");
         }
+
+
+
+        private void Delete_Button_Click(object sender, RoutedEventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(dbConnectionString);
+            try
+            {
+                conn.Open();
+                string query = "DELETE FROM Vehicles WHERE regNr='" + this.regText.Text+"'";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Deleted");
+                FillDataGrid();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
         /*
         private void Search_Button_Click(object sender, RoutedEventArgs e)
         {
@@ -227,7 +248,6 @@ namespace VeiebryggeApplication
             }
         }
     
-
 
         /*
         private void Update_Button_Click(object sender, RoutedEventArgs e)
