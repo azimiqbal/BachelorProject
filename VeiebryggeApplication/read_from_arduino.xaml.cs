06d00ce0424f17f4b19e3beabbc23517658610c8
azimiqbal <47232822+azimiqbal@users.noreply.github.com>
2021-03-18 10:57:36 +0100

    communicaton with arduino
    Added 2 pages in which we can read/write data to/from arduino.

---
+++ VeiebryggeApplication/read_from_arduino.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO.Ports;
+
+namespace VeiebryggeApplication
+{
+    /// <summary>
+    /// Interaction logic for read_from_arduino.xaml
+    /// </summary>
+    public partial class read_from_arduino : Page
+    {
+        public read_from_arduino()
+        {
+            InitializeComponent();
+
+        }
+
+        public void Button_Click(object sender, RoutedEventArgs e)
+        {
+            SerialPort sp = new SerialPort();
+            sp.BaudRate = 9600;
+            sp.PortName = "COM4";
+            sp.Open();
+            while (true)
+            {
+                string data_rx = sp.ReadLine();
+                Console.WriteLine(data_rx);
+            }
+        }
+    }
+
+}
+
+
+
+
